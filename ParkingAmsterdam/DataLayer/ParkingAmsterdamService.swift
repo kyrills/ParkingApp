f2b12b5e7de98c6bf7a36984cacc8a29e602445e
Trym Lintzen <Trymlintzen@Macintosh-van-Trym.local>
2017-11-07 13:03:13 +0100

    update 2 - parse data

--- ParkingAmsterdam/DataLayer/ParkingAmsterdamService.swift
+++ ParkingAmsterdam/DataLayer/ParkingAmsterdamService.swift
@@ -15,11 +15,12 @@ class ParkingAmsterdamService {
     
     private init() { // Singleton: https://en.wikipedia.org/wiki/Singleton_pattern
     }
+
     
-    func searchDiveMap()  {
+    func getParkingData()  {
         
-        let urlString =  "\(urls.baseURL)"
-        Alamofire.request( urlString ,
+        let ParkingData =  "\(urls.baseURL)"
+        Alamofire.request( ParkingData ,
                            method: .get,
                            parameters: nil,
                            encoding: JSONEncoding.default).validate().responseJSON { (response) in
@@ -33,7 +34,7 @@ class ParkingAmsterdamService {
                             case .failure(let error):
                                 print(error)
                             }
-
+                            
         }
     }
     
@@ -46,38 +47,38 @@ class ParkingAmsterdamService {
                     let geometry = dict["geometry"] as? NSDictionary,
                     let properties = dict["properties"] as? NSDictionary,
                     let id = dict["Id"] as? String {
-
-                        if let coordinates: NSArray = geometry["coordinates"] as? NSArray,
-                            let latitude = coordinates[0] as? Double,
-                            let longitude = coordinates[1] as? Double{
-                            
-                            
-                            if let name = properties["Name"] as? String,
-                                let pubDate = properties["PubDate"] as? String,
-                                let state = properties["State"] as? String,
-                                let freeSpaceShort = properties["FreeSpaceShort"] as? Int,
-                                let freeSpaceLong = properties["FreeSpaceLong"] as? Int,
-                                let shortCapacity = properties["ShortCapacity"] as? Int,
-                                let longCapacity = properties["LongCapacity"] as? Int{
-                                let parkingAppObject = ParkingObjects.init(id: id, latitude: latitude, longitude: longitude, Name: name, PubDate: pubDate, State: state, FreeSpaceShort: freeSpaceShort, FreeSpaceLong: freeSpaceLong, ShortCapacity: shortCapacity, LongCapacity: longCapacity)
-                                parkingObj.append(parkingAppObject)
-
-                            }
-                        }
                     
-
+                    if let coordinates: NSArray = geometry["coordinates"] as? NSArray,
+                        let latitude = coordinates[0] as? Double,
+                        let longitude = coordinates[1] as? Double{
+                        
+                        
+                        if let name = properties["Name"] as? String,
+                            let pubDate = properties["PubDate"] as? String,
+                            let state = properties["State"] as? String,
+                            let freeSpaceShort = properties["FreeSpaceShort"] as? Int,
+                            let freeSpaceLong = properties["FreeSpaceLong"] as? Int,
+                            let shortCapacity = properties["ShortCapacity"] as? Int,
+                            let longCapacity = properties["LongCapacity"] as? Int{
+                            let parkingAppObject = ParkingObjects.init(id: id, latitude: latitude, longitude: longitude, Name: name, PubDate: pubDate, State: state, FreeSpaceShort: freeSpaceShort, FreeSpaceLong: freeSpaceLong, ShortCapacity: shortCapacity, LongCapacity: longCapacity)
+                            parkingObj.append(parkingAppObject)
+                            
+                        }
                     }
+                    
+                    
                 }
-                NotificationCenter.default.post(name: NSNotification.Name(rawValue: NotificationID.setInitialData),
-                                                object: self,
-                                                userInfo: ["data" : parkingObj])
-
             }
-            
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: NotificationID.setInitialData),
+                                            object: self,
+                                            userInfo: ["data" : parkingObj])
             
         }
+        
+        
     }
-    
-    
-    
+}
+
+
+
 
